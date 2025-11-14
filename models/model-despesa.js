3adc60f6a1573b0f433577fb7b9556beb5b782dd
marcosliarte <marcos.liarte.neves@gmail.com>
2025-11-14 09:48:34 -0400

    atualização do sistema crashado

---
+++ models/model-despesa.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const DespesaSchema = new mongoose.Schema({
+    descricao: { type: String, required: true, trim: true },
+    valor: { type: Number, required: true },
+    data: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Despesa', DespesaSchema);
